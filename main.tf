cb3797cb32b315c537e42c727cd0cad7a1a787e9
KangAdit26 <kangadit2626@gmail.com>
2025-06-11 21:10:40 +0700

    Initial commit

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 data "aws_ami" "ubuntu" {
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
     
-    owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] # Canonical
 }
 
 provider "aws" {
@@ -19,9 +19,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-001f2488b35ca8aad"
+  ami           = "ami-001f2488b35ca8aad" # Pastikan AMI ini tersedia di region ap-southeast-2
   instance_type = "t2.micro"
-  key_name      = "PSO B Kelompok 13"
+  key_name      = "PSO B Kelompok 13" # Pastikan key pair ini sudah ada di AWS
 
   tags = {
     Name = "to-do-list"
