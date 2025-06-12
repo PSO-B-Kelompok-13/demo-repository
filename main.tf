f41327bc40e3ed265eb462ee622937f6b3fd8669
viexxxx <viqialvi11@gmail.com>
2025-06-12 20:06:15 +0700

    update main tf

--- main.tf
+++ main.tf
@@ -1,3 +1,55 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+# Security Group to allow SSH (22) and HTTP (3000) access
+resource "aws_security_group" "app_sg" {
+  name        = "app_security_group"
+  description = "Allow SSH and HTTP access"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP on port 3000"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg"
+  }
+}
+
+
 provider "aws" {
   region = var.aws_region
 }
